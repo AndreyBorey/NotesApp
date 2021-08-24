61316918864b2f2f3008b83dc23daa832e39de61
AndreyBorey <borisov.borey.andrey@gmail.com>
2021-08-24 10:40:22 +0300

    1st commit

---
+++ app/src/main/java/com/andreyborey/notes/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.andreyborey.notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mToolbar: Toolbar
+    private lateinit var mNavController: NavController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
